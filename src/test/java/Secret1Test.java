0536ca14a4abe3972326987e69b7a993743ae3ff
Mel Vasu <malavika.vasudevan@myob.com>
2018-06-08 08:39:26 +1000

    Updated secret equality test

--- src/test/java/Secret1Test.java
+++ src/test/java/Secret1Test.java
@@ -5,11 +5,9 @@ import static org.junit.Assert.assertEquals;
 public class Secret1Test {
 
     @Test
-    public void secret() {
+    public void secretWithSameNumberAreEqual() {
         Secret3 s1 = new Secret3(1234);
-        Secret3 s2 = new Secret3();
+        Secret3 s2 = new Secret3(1234);
         assertEquals(s1, s2);
     }
-
-
 }
