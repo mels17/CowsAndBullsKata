4dfce525009cddd09a3aa6e51508abc4294f6ff5
Mel Vasu <malavika.vasudevan@myob.com>
2018-06-08 09:08:49 +1000

    Added more tests and user validation

--- src/test/java/Secret1Test.java
+++ src/test/java/Secret1Test.java
@@ -1,13 +1,26 @@
 import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotEquals;
 
 public class Secret1Test {
 
     @Test
-    public void secretWithSameNumberAreEqual() {
+    public void secretWithSameNumberAreEqual() throws Exception {
         Secret3 s1 = new Secret3(1234);
         Secret3 s2 = new Secret3(1234);
         assertEquals(s1, s2);
+    }
+
+    @Test
+    public void secretsCanBeRandomlyGenerated() {
+        Secret3 s1 = new Secret3();
+        Secret3 s2 = new Secret3();
+        assertNotEquals(s1, s2);
+    }
+
+    @Test(expected = Exception.class)
+    public void secretWithDuplicatesWillThrow() throws Exception {
+        Secret3 s1 = new Secret3(1111);
     }
 }
